d167d86b8804519b036420a509c6a55a80467709
magklax <nadic2006@yandex.ru>
2021-01-04 09:22:12 +0100

    feat: add 3 walls

--- src/js/components/room.js
+++ src/js/components/room.js
@@ -1,13 +1,35 @@
-import lockedDoorSound from './../../assets/audio/locked-door.wav';
+const leftArrow = document.querySelector('#room-arrow-left');
+const rightArrow = document.querySelector('#room-arrow-right');
 
-const knob = document.querySelector('#knob');
-const audio = new Audio(lockedDoorSound);
+const walls = [
+    document.querySelector('#wall-1'),
+    document.querySelector('#wall-2'),
+    document.querySelector('#wall-3'),
+    document.querySelector('#wall-4')
+];
 
-const onKnobClick = () => {
-    knob.classList.remove('rotate');
-    void knob.offsetWidth;
-    knob.classList.add('rotate');
-    audio.play();
+class Room {
+    constructor() {
+        this.activeWall = walls[0];
+    }
+
+    init() {
+        leftArrow.addEventListener('click', () => {
+            this.activeWall.classList.remove('active');
+            let index = walls.indexOf(this.activeWall);
+            this.activeWall = index > 0 ? walls[index - 1] : walls[walls.length - 1];
+            this.activeWall.classList.add('active');
+        });
+
+        rightArrow.addEventListener('click', () => {
+            this.activeWall.classList.remove('active');
+            let index = walls.indexOf(this.activeWall);
+            this.activeWall = index < walls.length - 1 ? walls[index + 1] : walls[0];
+            this.activeWall.classList.add('active');
+        });
+    }
 }
 
-knob.addEventListener('click', onKnobClick);
+window.addEventListener('DOMContentLoaded', () => {
+    new Room().init();
+});
--- src/js/index.ts
+++ src/js/index.ts
@@ -4,3 +4,4 @@ import '../assets/scss/main.scss';
 // import 'materialize-css/dist/css/materialize.min.css';
 
 import './components/room';
+import './components/door';
