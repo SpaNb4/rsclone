f0f0ca209007d941edf8a8a7ccabe74f261b18c1
LemeshkoNastya <anastasia.lemeshko23@gmail.com>
2021-01-18 19:33:29 +0400

    feat: add adaptive, audio and code word game tetris

--- src/js/components/tetris.js
+++ src/js/components/tetris.js
@@ -1,26 +1,32 @@
 
 import {createBoard, drawBoard, piece, gameOver, clear} from './tetrisGame.js'
+import { playAudio } from './utils';
+import soundClickTetris from './../../assets/audio/tetris-click.mp3';
 
 const cubeImage = '.cube4',
       close = '.tetris__close',
       game = '.tetris',
       repeat = '.tetris__repeat',
-      active = 'active';
+      active = 'active',
+      audioClickTetris = new Audio(soundClickTetris);
 
 let dropStart = Date.now();
 
 document.querySelector(cubeImage).addEventListener('click', () => {
     document.querySelector(game).classList.add(active);
     startTetris();
+    playAudio(audioClickTetris);
 });
 
 document.querySelector(close).addEventListener('click', () => {
     document.querySelector(game).classList.remove(active);
+    playAudio(audioClickTetris);
     
 });
 
 document.querySelector(repeat).addEventListener('click', () => {
     startTetris();
+    playAudio(audioClickTetris);
 });
 
 const startTetris = () => {
@@ -36,17 +42,21 @@ const KeyDown = (event) => {
         case 37:
             piece.moveLeft();
             dropStart = Date.now();
+            playAudio(audioClickTetris);
             break;
         case 38:
             piece.rotate();
             dropStart = Date.now();
+            playAudio(audioClickTetris);
             break;
         case 39:
             piece.moveRight();
             dropStart = Date.now();
+            playAudio(audioClickTetris);
             break;
         case 40:
             piece.moveDown();
+            playAudio(audioClickTetris);
             break;
     }
 }
